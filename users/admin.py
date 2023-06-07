cf0624aa56582f32345fad52ebabbc940939a04a
Ali Ghaffari Sabet <aghs8055@gmail.com>
2023-06-08 00:12:30 +0330

    Admin panel for custom user configured.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from users.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Profile', {'fields': ('user_type',)}),
+    )
