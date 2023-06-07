6c11ed0deeb3df95b8d02cf77d9552edd6d18bf2
Ali Ghaffari Sabet <aghs8055@gmail.com>
2023-06-08 01:48:50 +0330

    Login view added.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth import authenticate
+from django.shortcuts import redirect
+from django.urls import reverse
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'users/login.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            return redirect(reverse('pages:home'))
+        else:
+            return render(request, 'users/login.html', {'message': 'Invalid credentials'})
