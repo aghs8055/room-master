449aa184d1207ecec6f19b3349c9aeb3f5d0030e
Ali Ghaffari Sabet <aghs8055@gmail.com>
2023-06-08 17:22:26 +0330

    Page and view related to room creation added.

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,14 +4,16 @@ from rooms.views import (
     RequestListView,
     RequestCreateView,
     RequestDeleteView,
-    RoomListView
+    RoomListView,
+    RoomCreateView,
 )
 
 app_name = 'rooms'
 
 urlpatterns = [
-    path('requests', RequestListView.as_view(), name='request_list'),
-    path('create/', RequestCreateView.as_view(), name='request_create'),
+    path('requests/', RequestListView.as_view(), name='request_list'),
+    path('requests/create/', RequestCreateView.as_view(), name='request_create'),
     path('<int:request_id>/delete/', RequestDeleteView.as_view(), name='request_delete'),
     path('', RoomListView.as_view(), name='room_list'),
+    path('create/', RoomCreateView.as_view(), name='room_create'),
 ]
