e7d2b8693b4a16586aa85fee936e63c435ec82be
Ali Ghaffari Sabet <aghs8055@gmail.com>
2023-06-08 19:13:47 +0330

    A bug in checking type of user in AdminOrManagerAccessMixin fixed.

--- users/mixins.py
+++ users/mixins.py
@@ -1,13 +1,16 @@
 from django.shortcuts import redirect
 from django.urls import reverse
 
+from users.models import User
+
+
 class AdminOrManagerAccessMixin:
     redirect_url = 'pages:not_found'
     login_url = 'users:login'
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            if request.user.is_admin or request.user.is_manager:
+            if request.user.user_type in [User.UserType.ADMIN, User.UserType.MANAGER]:
                 return super().dispatch(request, *args, **kwargs)
             else:
                 return redirect(reverse(self.redirect_url))
