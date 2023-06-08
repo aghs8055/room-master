da9fe362a06fd6e43708145f961448109f73d1bb
Ali Ghaffari Sabet <aghs8055@gmail.com>
2023-06-08 16:58:57 +0330

    HTML page and view for room list added.

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from rooms.views import RequestListView, RequestCreateView, RequestDeleteView
+from rooms.views import (
+    RequestListView,
+    RequestCreateView,
+    RequestDeleteView,
+    RoomListView
+)
 
 app_name = 'rooms'
 
 urlpatterns = [
-    path('', RequestListView.as_view(), name='request_list'),
+    path('requests', RequestListView.as_view(), name='request_list'),
     path('create/', RequestCreateView.as_view(), name='request_create'),
     path('<int:request_id>/delete/', RequestDeleteView.as_view(), name='request_delete'),
+    path('', RoomListView.as_view(), name='room_list'),
 ]
