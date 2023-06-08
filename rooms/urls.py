9d3d829b4d7cb42f146943ed96d84d23f73cdcfa
Ali Ghaffari Sabet <aghs8055@gmail.com>
2023-06-08 16:33:07 +0330

    Request delete view added and its link added to request list page.

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from rooms.views import RequestListView, RequestCreateView
+from rooms.views import RequestListView, RequestCreateView, RequestDeleteView
 
 app_name = 'rooms'
 
 urlpatterns = [
     path('', RequestListView.as_view(), name='request_list'),
     path('create/', RequestCreateView.as_view(), name='request_create'),
+    path('<int:request_id>/delete/', RequestDeleteView.as_view(), name='request_delete'),
 ]
