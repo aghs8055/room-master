45c496f354c9a065ac64e39346c6f4f1c1cd871c
Ali Ghaffari Sabet <aghs8055@gmail.com>
2023-06-08 11:06:44 +0330

    HTML page and view for user signup added.

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from users.views import LoginView
+from users.views import LoginView, SignupView
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('signup/', SignupView.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import View
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
 from django.urls import reverse
+
+from users.forms import SignupForm
 
 
 class LoginView(View):
@@ -14,6 +16,22 @@ class LoginView(View):
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user:
+            login(request, user)
             return redirect(reverse('pages:home'))
         else:
             return render(request, 'users/login.html', {'credentials': 'Invalid credentials'})
+        
+
+class SignupView(View):
+    def get(self, request):
+        return render(request, 'users/signup.html')
+    
+    def post(self, request):
+        user_form = SignupForm(data=request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect(reverse('users:login'))
+        else:
+            print(request.POST)
+            print(dict(user_form.errors))
+            return render(request, 'users/signup.html', {'errors': dict(user_form.errors)})
