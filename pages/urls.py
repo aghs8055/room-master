bcdc3896a8e658b908750dc36da2b85fe637945a
Ali Ghaffari Sabet <aghs8055@gmail.com>
2023-06-08 16:21:40 +0330

    Not found HTML page and view added.

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from pages.views import HomeView
+from pages.views import HomeView, NotFoundView
 
 app_name = 'pages'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('not-found/', NotFoundView.as_view(), name='not_found'),
 ]
