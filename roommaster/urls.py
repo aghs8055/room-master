435a5fd871b488c501787672e2b8ed753726d7ef
Ali Ghaffari Sabet <aghs8055@gmail.com>
2023-06-08 13:15:11 +0330

    Rooms app created and configured.

--- roommaster/urls.py
+++ roommaster/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('users/', include('users.urls')),
+    path('rooms/', include('rooms.urls')),
 ]
