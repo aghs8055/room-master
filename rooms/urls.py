7a3f2fb687bf50356ffb82936cc6edf4d8a49e15
Ali Ghaffari Sabet <aghs8055@gmail.com>
2023-06-08 15:28:39 +0330

    View and HTML page for room request listing added.

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+
+from rooms.views import RequestListView
+
 app_name = 'rooms'
 
-urlpatterns = []
+urlpatterns = [
+    path('', RequestListView.as_view(), name='request_list'),
+]
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from rooms.models import Request
+from users.models import User
+
+
+class RequestListView(View):
+    def get(self, request):
+        if request.user.user_type == User.UserType.USER:
+            room_requests = Request.objects.filter(user=request.user).order_by('-created_at')
+            return render(request, 'rooms/request_list.html', {'room_requests': room_requests})
+        else:
+            room_requests = Request.objects.all().order_by('-created_at')
+            return render(request, 'rooms/request_list.html', {'room_requests': room_requests})
+        
