12191a1594835d35e275b8c2a44a4f16c225bd98
Ali Ghaffari Sabet <aghs8055@gmail.com>
2023-06-09 22:53:06 +0330

    Unused admin type removed from user model.

--- users/models.py
+++ users/models.py
@@ -5,18 +5,9 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import UserManager as DjangoUserManager
 
 
-class UserManager(DjangoUserManager):
-    def create_superuser(self, username, email, password, **extra_fields):
-        extra_fields.setdefault('user_type', User.UserType.ADMIN)
-        return super().create_superuser(username, email, password, **extra_fields)
-
-
 class User(AbstractUser):
     class UserType(models.TextChoices):
         USER = 'user', _('User')
-        ADMIN = 'admin', _('Admin')
         MANAGER = 'manager', _('Manager')
 
     user_type = models.CharField(max_length=7, choices=UserType.choices, default=UserType.USER)
-
-    objects = UserManager()
